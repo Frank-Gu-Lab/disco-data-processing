656767cd1ea466d417a476e64c7aca8729138185
hdmd2003 <67080763+hdmd2003@users.noreply.github.com>
2022-05-27 12:57:32 -0400

    Testing for data_checker.py

---
+++ tests/test_data_checker.py
@@ -0,0 +1,22 @@
+import pytest
+import sys
+import os
+import glob
+import shutil
+
+# appending path to access sibling directory - uncomment if local package setup doesn't work
+sys.path.append(os.getcwd() + '/../src')
+
+from discoprocess.data_checker import *
+
+def test_name_checker():
+
+    pass 
+
+def test_resonance_and_column_checker():
+
+    pass
+
+def test_range_checker():
+
+    pass
