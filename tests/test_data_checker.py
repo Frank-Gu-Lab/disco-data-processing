3b80fb129edb278d4fffe09b824972cc14db81b6
hdmd2003 <67080763+hdmd2003@users.noreply.github.com>
2022-05-27 13:41:55 -0400

    Testing for the new input checking module, data_check.py

--- tests/test_data_checker.py
+++ tests/test_data_checker.py
@@ -3,20 +3,44 @@ import sys
 import os
 import glob
 import shutil
+import unittest
 
 # appending path to access sibling directory - uncomment if local package setup doesn't work
 sys.path.append(os.getcwd() + '/../src')
 
 from discoprocess.data_checker import *
 
-def test_name_checker():
+class TestChecks:
 
-    pass 
+    def test_name_checker(self):
 
-def test_resonance_and_column_checker():
+        list_of_raw_books = []
 
-    pass
+        list_of_raw_books.append("C:/Users/matth/OneDrive/Documents/GitHub/disco-data-processing/tests/test-files/test_checker/test_checker_input/Batch-CMC_131k_20uM.xlsx")
 
-def test_range_checker():
+        with pytest.raises(Exception) as e:
+            name_checker(list_of_raw_books)
 
-    pass
+        assert e.match("Please add 'uM' to the end of the concentration in the polymer name")
+
+    def test_resonance_and_column_checker(self):
+
+                list_of_raw_books = []
+
+                list_of_raw_books.append("C:/Users/matth/OneDrive/Documents/GitHub/disco-data-processing/tests/test-files/test_checker/test_checker_input/Batch-CMC_90k_20uM.xlsx")
+
+                with pytest.raises(Exception) as e:
+                    resonance_and_column_checker(list_of_raw_books)
+
+                assert e.match("Please ensure that all odd replicates are On resonance")
+
+    def test_range_checker(self):
+
+        list_of_raw_books = []
+
+        list_of_raw_books.append("C:/Users/matth/OneDrive/Documents/GitHub/disco-data-processing/tests/test-files/test_checker/test_checker_input/Batch-PEG_2k_20uM.xlsx")
+
+        with pytest.raises(Exception) as e:
+            range_checker(list_of_raw_books)
+
+        assert e.match("Please ensure the ranges are equivalent across all tables")
