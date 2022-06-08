cd53eeea0cfc969510218668b11c4a62744188fb
hdmd2003 <67080763+hdmd2003@users.noreply.github.com>
2022-06-08 10:31:06 -0400

    Update dashboard.py

--- src/dashboard.py
+++ src/dashboard.py
@@ -41,19 +41,113 @@ global_output_directory_1 = st.sidebar.text_input("Directory: ")
 
 list_of_raw_books = []
 
-if global_output_directory_1 != None:
-    st.info("Would you like to upload data for data analysis, or plot data from the directory specified?")
+choice = None
 
-    choice = st.radio(["Upload and analyze", "Plot existing data"])
+def analysis(list_of_raw_books):
+    st.info("Checking names of polymers in datasets for correct formatting")
+
+    i = 0
+
+    if name_checker(list_of_raw_books):
+        st.success("Names are formatted correctly!")
+        i += 1
+
+    if resonance_and_column_checker(list_of_raw_books):
+        st.success("Data formatted correctly!")
+        i += 1
+
+    if range_checker(list_of_raw_books):
+        st.success("Data ranges are all correct!")
+        i += 1
+
+    if i == 3:
+        st.info("Great! Now it is time for data analysis!")
+        i += 1
+
+    batch_tuple_list = []
+
+    clean_batch_tuple_list = []
+    clean_batch_list = []
+
+    for book in list_of_raw_books:
+        #append tuples from the list output of the batch processing function, so that each unique polymer tuple is assigned to the clean_batch_tuple_list
+        batch_tuple_list.append([polymer for polymer in batch_to_dataframe(book)])
+
+    # PERFORM DATA CLEANING ON ALL BOOKS PROCESSED VIA BATCH PROCESSING ----------------
+
+    #if there has been batch data processing, call the batch cleaning function
+    if len(batch_tuple_list) != 0:
+
+        clean_batch_list = clean_the_batch_tuple_list(batch_tuple_list)
+
+        # convert clean batch list to a clean batch tuple list format (polymer_name, df) for further processing
+        clean_batch_tuple_list = [(clean_batch_list[i]['polymer_name'].iloc[0], clean_batch_list[i]) for i in range(len(clean_batch_list))]
+
+    # LOOP THROUGH AND PROCESS EVERY CLEAN DATAFRAME IN THE BATCH LIST GENERATED ABOVE, IF ANY ----------------------------------
+
+    if len(clean_batch_tuple_list) != 0:
+        with st.spinner("Analyzing data..."):
+            analyze_data(clean_batch_tuple_list, global_output_directory)
+            i += 1
+
+    if i == 5:
+        return
+
+if len(global_output_directory_1) > 0:
+
+    choice = st.radio("Would you like to upload data for data analysis, or plot data from the directory specified?", ["Upload and analyze", "Plot existing data"])
 
 if choice == "Upload and analyze":
     if global_output_directory_1 != None and global_output_directory_1 != "":
         global_output_directory = "../data/output/" + global_output_directory_1
         if not os.path.exists(global_output_directory):
             os.makedirs(global_output_directory)
-        st.success("Thank you!  Please upload your data files to begin data processing!")
+        st.info("Please upload your data files to begin data processing!")
         list_of_raw_books = st.sidebar.file_uploader("Please provide input files", accept_multiple_files = True)
 
     i = 0
+
+
+    if len(list_of_raw_books) > 0:
+        analysis(list_of_raw_books)
+        i += 5
+
+
+    if i == 5:
+        st.success("All polymers successfully analyzed")
+        sht.make_archive(os.path.abspath('../data/output/' + global_output_directory_1), 'zip', '../data/output/',os.path.abspath(global_output_directory))
+        with open('../data/output/' + global_output_directory_1 + '.zip', 'rb') as f:
+           st.download_button('Download Zip with Analyzed Data', f, file_name=global_output_directory_1+'.zip')
+           i += 1
+
+    if i == 6:
+        # makes a global ouput directory for merged data if not existing
+        merge_output_directory = "{}/merged".format(global_output_directory)
+
+        if not os.path.exists(merge_output_directory):
+            os.makedirs(merge_output_directory)
+        # define data source path and data destination path to pass to data merging function
+        source_path = '{}/*/tables_*'.format(global_output_directory)
+        destination_path = '{}'.format(merge_output_directory)
+
+        list_of_merging_stuff = glob.glob(source_path + "/*")
+
+        try:
+
+            replicate_summary_df = etl_per_replicate(source_path, destination_path)
+            rep_sum = replicate_summary_df.to_excel(os.path.join(merge_output_directory, "merged_binding_dataset.xlsx"))
+
+            quality_check_df = etl_per_sat_time(source_path, destination_path)
+            qual_check = quality_check_df.to_excel(os.path.join(merge_output_directory, "merged_fit_quality_dataset.xlsx"))
+
+            proton_summary_df = etl_per_proton(quality_check_df)
+            prot_sum = proton_summary_df.to_excel(os.path.join(merge_output_directory, "proton_binding_dataset.xlsx"))
+
+            sht.make_archive(os.path.abspath(merge_output_directory), "zip", global_output_directory, os.path.abspath(merge_output_directory))
+            with open(merge_output_directory + ".zip", "rb") as f:
+                st.download_button("Press to download merged datesets", f, file_name = "merged" + ".zip")
+                i = i + 1
+        except ValueError:
+            st.info("There were no binding polymers, please rerun with a new dataset to try other samples! (simply upload more to begin the process)")
 else:
     pass
