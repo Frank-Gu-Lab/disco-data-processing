ea293936a5ff06e7f28c2be008528a041e618656
hdmd2003 <67080763+hdmd2003@users.noreply.github.com>
2022-06-15 13:31:13 -0400

    test_barlett() implemented

--- tests/test_wrangle_data.py
+++ tests/test_wrangle_data.py
@@ -69,15 +69,74 @@ def test_calculate_abs_buildup_params():
 
 def test_calculate_buildup_params():
 
-    pass
+    mean_df =  pd.read_excel(input_path + "/stats_analysis_output_mean_" + "CMC_90k_20uM" + ".xlsx", index_col=[0, 1, 2, 3], header=[0, 1]).reset_index()
+
+    colnames = mean_df.columns.get_level_values(0).values
+    mean_df = mean_df.droplevel(1, axis=1)
+    colnames[4] = "corr_%_attenuation_mean"
+    colnames[5] = "corr_%_attenuation_std"
+    mean_df.columns = colnames
+
+    actual_1 = mean_df['sat_time'].values
+    actual_2 = mean_df['corr_%_attenuation_mean'].values
+    std = mean_df['corr_%_attenuation_std'].values
+    n = mean_df['sample_size'].values
+    std_err = std/np.sqrt(n)
+
+    actual_3 = np.subtract(actual_2, std_err)
+    actual_4 = np.add(actual_2, std_err)
+
+    expected_1, expected_2, expected_3, expected_4 = calculate_buildup_params(mean_df)
+
+    np.testing.assert_array_equal(actual_1, expected_1)
+    np.testing.assert_array_equal(actual_2, expected_2)
+    np.testing.assert_array_equal(actual_3, expected_3)
+    np.testing.assert_array_equal(actual_4, expected_4)
 
 def test_shapiro_wilk():
 
-    pass
+    mean_df =  pd.read_excel(input_path + "/stats_analysis_output_mean_" + "CMC_90k_20uM" + ".xlsx", index_col=[0, 1, 2, 3], header=[0, 1]).reset_index()
+
+    colnames = mean_df.columns.get_level_values(0).values
+    mean_df = mean_df.droplevel(1, axis=1)
+    colnames[4] = "corr_%_attenuation_mean"
+    colnames[5] = "corr_%_attenuation_std"
+    mean_df.columns = colnames
+
+    x = mean_df['sat_time'].values
+
+    actual = shapiro_wilk(x)
+    stat, expected = stats.shapiro(x)
+
+    assert actual == expected
 
 def test_barlett():
 
-    pass
+    mean_df =  pd.read_excel(input_path + "/stats_analysis_output_mean_" + "CMC_90k_20uM" + ".xlsx", index_col=[0, 1, 2, 3], header=[0, 1]).reset_index()
+
+    colnames = mean_df.columns.get_level_values(0).values
+    mean_df = mean_df.droplevel(1, axis=1)
+    colnames[4] = "corr_%_attenuation_mean"
+    colnames[5] = "corr_%_attenuation_std"
+    mean_df.columns = colnames
+
+    x = mean_df['sat_time'].values
+
+    mean_df_2 =  pd.read_excel(input_path + "/stats_analysis_output_mean_" + "PVA86-89_105k_20uM" + ".xlsx", index_col=[0, 1, 2, 3], header=[0, 1]).reset_index()
+
+    colnames = mean_df_2.columns.get_level_values(0).values
+    mean_df = mean_df_2.droplevel(1, axis=1)
+    colnames[4] = "corr_%_attenuation_mean"
+    colnames[5] = "corr_%_attenuation_std"
+    mean_df_2.columns = colnames
+
+    y = mean_df_2['sat_time'].values
+
+    actual = bartlett(x, y)
+
+    stat, expected = stats.bartlett(x, y)
+
+    assert actual == expected
 
 def test_change_significance():
 
